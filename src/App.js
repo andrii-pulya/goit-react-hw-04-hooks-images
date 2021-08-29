2305be6aed78acdbdb80edb4cfb40b45dcc9d440
andrii-pulya <andrey.predein@gmail.com>
2021-08-29 18:16:39 +0300

    ready

--- src/App.js
+++ src/App.js
@@ -1,119 +1,121 @@
-import "./App.css";
+import './App.css'
 
-import React, { Component } from "react";
-import toast, { Toaster } from "react-hot-toast";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import React, { useState, useEffect } from 'react'
+import toast, { Toaster } from 'react-hot-toast'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 
-import Serchbar from "./components/Searchbar";
-import ImageGallery from "./components/ImageGallery.jsx";
-import ImageGalleryItem from "./components/ImageGalleryItem.jsx";
-import Modal from "./components/Modal.jsx";
-import Button from "./components/Button.jsx";
-import Loader from "react-loader-spinner";
-import PixabayServiseApi from "./services/apiService";
+import Serchbar from './components/Searchbar'
+import ImageGallery from './components/ImageGallery.jsx'
+import ImageGalleryItem from './components/ImageGalleryItem.jsx'
+import Modal from './components/Modal.jsx'
+import Button from './components/Button.jsx'
+import Loader from 'react-loader-spinner'
+import PixabayServiseApi from './services/apiService'
 
-const PixabayService = new PixabayServiseApi();
+const PixabayService = new PixabayServiseApi()
 
-export default class App extends Component {
-  state = {
-    pictureName: null,
-    pictures: [],
-    reqStatus: "idle", //idle, pending, resolved, rejected
-    showModal: false,
-    largeImageUrl: null,
-    imageTags: null,
-  };
+export default function App() {
+  const [pictureName, setPictureName] = useState(null)
+  const [pictures, setPictures] = useState([])
+  const [reqStatus, setReqstatus] = useState('idle')
+  const [showModal, setShowModal] = useState(false)
+  const [largeImageUrl, setLargeImageUrl] = useState(null)
+  const [imageTags, setImageTags] = useState(null)
 
-  componentDidUpdate(_, prevState) {
-    const { pictureName } = this.state;
-    if (pictureName === "") {
-      toast.error("Please, enter the key word!");
+  useEffect(() => {
+    if (pictureName === null) {
+      return
+    }
+
+    if (pictureName === '') {
+      toast.error('Please, enter the key word!')
     } else {
-      if (prevState.pictureName !== pictureName) {
-        this.setState({ pictures: [], reqStatus: "pending" });
-        PixabayService.resetPage();
-        PixabayService.pictureFind(pictureName)
-          .then((response) => {
-            if (response.hits.length === 0) {
-              this.setState({ reqStatus: "rejected" });
-              toast.error("We tried, but can't find any");
-            } else {
-              this.setState({ pictures: response.hits, reqStatus: "resolved" });
-            }
-          })
-          .catch((error) => alert(error.message));
-      }
+      setPictures([])
+      setReqstatus('pending')
+      PixabayService.resetPage()
+      PixabayService.pictureFind(pictureName)
+        .then((response) => {
+          if (response.hits.length === 0) {
+            setReqstatus('rejected')
+            toast.error("We tried, but can't find any")
+          } else {
+            setPictures(response.hits)
+            setReqstatus('resolved')
+          }
+        })
+        .catch((error) => alert(error.message))
+    }
+  }, [pictureName])
+
+  function handleFormSubmit(pictureNameFind) {
+    if (pictureNameFind.trim() !== '') {
+      setPictureName(pictureNameFind.trim())
+    } else {
+      toast.error('Please, enter the key word!')
+    }
+
+    if (pictureNameFind.trim() === pictureName) {
+      toast('Look, We already find it!', {
+        style: { color: 'blue', backgroundColor: 'yellow' },
+        icon: '🔥',
+      })
     }
   }
 
-  handleFormSubmit = (pictureName) => {
-    this.setState({ pictureName: pictureName.trim() });
-    if (pictureName.trim() === this.state.pictureName) {
-      toast("Look, We already find it!", {
-        style: { color: "blue", backgroundColor: "yellow" },
-        icon: "🔥",
-      });
-    }
-  };
-
-  handleLoadMore = () => {
-    this.setState({ reqStatus: "pending" });
-    PixabayService.incrementPage();
-    PixabayService.pictureFind(this.state.pictureName)
+  function handleLoadMore() {
+    setReqstatus('pending')
+    PixabayService.incrementPage()
+    PixabayService.pictureFind(pictureName)
       .then((response) => {
-        const newImages = response.hits;
-        this.setState({
-          pictures: [...this.state.pictures, ...newImages],
-        });
+        const newImages = response.hits
+        setPictures([...pictures, ...newImages])
         window.scrollTo({
           top: document.documentElement.scrollHeight,
-          behavior: "smooth",
-        });
+          behavior: 'smooth',
+        })
       })
-      .catch((error) => alert(error.message));
-    this.setState({ reqStatus: "resolved" });
-  };
-
-  toggleModal = (largeImageURL, imageTags) => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
-    this.setState({ largeImageUrl: largeImageURL, imageTags });
-  };
-
-  render() {
-    const { pictures, reqStatus, showModal, largeImageUrl, imageTags } =
-      this.state;
-    const showGallery = pictures.length > 1;
-    const showLoadMoreBtn = pictures.length >= 12;
-    return (
-      <div>
-        <Serchbar onSubmit={this.handleFormSubmit} />
-
-        {showGallery && (
-          <ImageGallery>
-            <ImageGalleryItem pictures={pictures} onClick={this.toggleModal} />
-          </ImageGallery>
-        )}
-        {reqStatus === "pending" && (
-          <div className="loaderContainer">
-            <Loader
-              type="ThreeDots"
-              color="#00BFFF"
-              height={100}
-              width={100}
-              timeout={3000}
-            />
-          </div>
-        )}
-        {showLoadMoreBtn && <Button onClick={this.handleLoadMore} />}
-        {showModal && (
-          <Modal
-            largeImg={largeImageUrl}
-            tags={imageTags}
-            onClose={this.toggleModal}
-          />
-        )}
-        <Toaster position="top-right" />
-      </div>
-    );
+      .catch((error) => alert(error.message))
+    setReqstatus('resolved')
   }
+
+  function toggleModal(largeImageURL, imageTags) {
+    setShowModal(!showModal)
+    setLargeImageUrl(largeImageURL)
+    setImageTags(imageTags)
+  }
+
+  const showGallery = pictures.length > 1
+  const showLoadMoreBtn = pictures.length >= 12
+
+  return (
+    <div>
+      <Serchbar onSubmit={handleFormSubmit} />
+
+      {showGallery && (
+        <ImageGallery>
+          <ImageGalleryItem pictures={pictures} onClick={toggleModal} />
+        </ImageGallery>
+      )}
+      {reqStatus === 'pending' && (
+        <div className="loaderContainer">
+          <Loader
+            type="ThreeDots"
+            color="#00BFFF"
+            height={100}
+            width={100}
+            timeout={3000}
+          />
+        </div>
+      )}
+      {showLoadMoreBtn && <Button onClick={handleLoadMore} />}
+      {showModal && (
+        <Modal
+          largeImg={largeImageUrl}
+          tags={imageTags}
+          onClose={toggleModal}
+        />
+      )}
+      <Toaster position="top-right" />
+    </div>
+  )
 }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,33 +1,32 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown)
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown)
-  }
+export default function Modal({ largeImg, tags, onClose }) {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown)
 
-  handleKeyDown = (e) => {
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  })
+
+  function handleKeyDown(e) {
     if (e.code === 'Escape') {
-      this.props.onClose()
+      onClose()
     }
   }
 
-  handleBackdropClick = (e) => {
-    if (e.currentTarget === e.target) this.props.onClose()
+  function handleBackdropClick(e) {
+    if (e.currentTarget === e.target) onClose()
   }
 
-  render() {
-    return (
-      <div className="Overlay" onClick={this.handleBackdropClick}>
-        <div className="Modal">
-          <img src={this.props.largeImg} alt={this.props.tags} />
-        </div>
+  return (
+    <div className="Overlay" onClick={handleBackdropClick}>
+      <div className="Modal">
+        <img src={largeImg} alt={tags} />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 Modal.propTypes = {
